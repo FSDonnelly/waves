d1e423471438e0a3c0e2e59b02182471ba60124f
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-28 20:08:09 -0700

    bcrypt.hash password for user register

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
+const SALT = 10;
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -39,6 +40,22 @@ const UserSchema = new mongoose.Schema({
   token: {
     type: String
   }
+});
+
+// Encrypt password
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  bcrypt.genSalt(SALT, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = User = mongoose.model('User', UserSchema);
