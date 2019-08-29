8b88fb04304abc9f517de99d301a802ea324bb81
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-28 18:09:50 -0700

    setup static assets in production

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-const connectDB = require('./db');
+const connectDB = require('./cofig/db');
 
 const app = express();
 
 // Connect Database
 connectDB();
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
