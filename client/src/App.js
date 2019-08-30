dee25226e73bcd1db6e48edd504a03a3db65f587
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-30 15:36:58 -0700

    set proxy in client package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('/api/product/brands').then(res => {
+      console.log(res);
+    });
+  }
+
   render() {
     return <div>MY APP</div>;
   }
