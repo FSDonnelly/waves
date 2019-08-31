0b7285c5719ffb2a5e3cfdbe7713012dd7e02008
FSDonnelly <donnelly_frank@yahoo.com>
2019-08-30 21:51:30 -0700

    started footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -5,16 +5,38 @@ const Footer = () => {
     <footer className='bck_b_dark'>
       <div className='container'>
         <div className='logo'>SOUNDWAVES</div>
+        <h2>Contact Information</h2>
         <div className='wrapper'>
-          <div className='left'>
-            <h2>Contact Information</h2>
-            <div className='business_nfo'>
-              <div className='tag'>
-                <i class='far fa-compass bg'></i>
+          <div className='business_nfo'>
+            <div className='tag'>
+              <i className='far fa-compass icon' />
+              <div className='nfo'>
+                <div>Address</div>
+                <div>Main St 1234</div>
+              </div>
+            </div>
+            <div className='tag'>
+              <i className='far fa-phone icon' />
+              <div className='nfo'>
+                <div>Phone</div>
+                <div>(555) 555 - 5555</div>
+              </div>
+            </div>
+            <div className='tag'>
+              <i className='far fa-clock icon' />
+              <div className='nfo'>
+                <div>Working Hours</div>
+                <div>9 AM to 10 PM Mon - Sat</div>
+              </div>
+            </div>
+            <div className='tag'>
+              <i className='far fa-envelope icon' />
+              <div className='nfo'>
+                <div>Email</div>
+                <div>swaves@fakemail.com</div>
               </div>
             </div>
           </div>
-          <div className='right'></div>
         </div>
       </div>
     </footer>
