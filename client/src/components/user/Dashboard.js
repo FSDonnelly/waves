55e262af4a325b9573302d3e35fc1b232a5230a7
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-02 18:16:37 -0700

    add DashboardLayout

--- client/src/components/user/Dashboard.js
+++ client/src/components/user/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import DashboardLayout from '../../hoc/DashboardLayout';
 import { loadUser } from '../../actions/user';
 
 const Dashboard = ({ loadUser, user: { loading, isAuthenticated } }) => {
@@ -13,21 +14,8 @@ const Dashboard = ({ loadUser, user: { loading, isAuthenticated } }) => {
     <div>Loading...</div>
   ) : (
     <Fragment>
-      <div className='container'>
-        <h1 className='large text-primary'>Dashboard</h1>
-        <p className='lead'>
-          <i className='fas fa-user' /> Welcome
-        </p>
-        {loadUser !== null ? (
-          <Fragment>
-            <div className='my-2'></div>
-          </Fragment>
-        ) : (
-          <Fragment>
-            <p>You have not yet setup a user, please add some info</p>
-          </Fragment>
-        )}
-      </div>
+      <div>Dashboard</div>
+      <DashboardLayout></DashboardLayout>
     </Fragment>
   );
 };
