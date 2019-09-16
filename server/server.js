bdc329706afadde6d9d86bb5d5fde1ff6d7cb138
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-15 17:21:28 -0700

    setup express server connected to DB

--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+// Setup servr
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+// Setup connection to DB
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE || process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+// Init middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 3002;
 
