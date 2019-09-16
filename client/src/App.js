d5db7ae89ea0dceae538960d54c3ff85cb733c7b
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-16 10:35:56 -0700

    setup concurrently to run front and back in root

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return <div className='App'>My App</div>;
+class App extends Component {
+  componentDidMount() {
+    axios.get('/api/product/brands').then(res => console.log(res));
+  }
+
+  render() {
+    return <div className='App'>My App</div>;
+  }
 }
 
 export default App;
