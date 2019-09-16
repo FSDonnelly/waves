8481f7ae4ccca16948e6350d4863190f4478793c
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-15 19:42:05 -0700

    add logic to login user

--- server/server.js
+++ server/server.js
@@ -23,13 +23,40 @@ const { User } = require('./models/user');
 //===========================
 //    USERS
 //===========================
-
+// Register User
 app.post('/api/users/register', (req, res) => {
   const user = new User(req.body);
 
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
     res.status(200).json({ success: true, userdata: doc });
+  });
+});
+
+// Login User
+app.post('/api/users/login', (req, res) => {
+  // find email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({ loginSuccess: false, message: 'Email not found' });
+    // check password
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: 'Password not valid'
+        });
+      // generate token
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res
+          .cookie('w_auth', user.token)
+          .status(200)
+          .json({
+            loginSuccess: true
+          });
+      });
+    });
   });
 });
 
